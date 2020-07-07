03b2eb3f6ca2ddbaf1f99445f3342f20e3ce01ac
tzuhaochu <tzuhaozhu@gmail.com>
2020-07-07 10:21:39 +0800

    完成grad部分的学习

--- test_grad.py
+++ test_grad.py
@@ -38,3 +38,41 @@ out3 = x.sum()
 x.grad.data.zero_()
 out3.backward()
 print(x.grad)
+
+x = torch.tensor([1.0, 2.0, 3.0, 4.0], requires_grad=True)
+y = 2 * x
+z = y.view(2, 2)
+print(z)
+
+v = torch.tensor([[1.0, 0.1],[0.01, 0.001]], dtype=torch.float)
+print(z * v)
+z.backward(v)
+print(x.grad)
+
+#这边没有grad为false是不会回传的
+x = torch.tensor(1.0, requires_grad=True)
+y1 = x ** 2
+with torch.no_grad():
+    y2 = x ** 3
+y3 = y1 + y2
+
+print(x.requires_grad)
+print(y1, y1.requires_grad)
+print(y2, y2.requires_grad)
+print(y3, y3.requires_grad)
+
+y3.backward()
+print(x.grad)
+
+
+#对tensor.data操作不会被记录在autograd中，但是还是会改变tensor的值
+x = torch.ones(1, requires_grad=True)
+print(x.data)
+print(x.data.requires_grad)
+
+y = 2 * x
+x.data *= 100
+
+y.backward()
+print(x)
+print(x.grad)
